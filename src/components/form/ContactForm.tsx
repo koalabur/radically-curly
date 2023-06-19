5945f59b5862b679c41d77668a2168adb426ddcc
Cameron Roberts <cameronrdesign@gmail.com>
2023-06-19 15:09:21 -0700

    chore: unit tests

--- src/components/form/ContactForm.tsx
+++ src/components/form/ContactForm.tsx
@@ -18,23 +18,36 @@ export default function Form() {
     <form onSubmit={handleSubmit} className={styles.ContactForm}>
       <div className={styles.ContactForm__row}>
         <label htmlFor="name">Name</label>
-        <input id="name" type="text" name="name" required />
+        <input id="name" type="text" name="name" aria-label="name" required />
         <ValidationError prefix="Name" field="name" errors={state.errors} />
       </div>
       <div className={styles.ContactForm__row}>
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" required />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          aria-label="email"
+          required
+        />
         <ValidationError prefix="Email" field="email" errors={state.errors} />
       </div>
       <div className={styles.ContactForm__row}>
         <label htmlFor="subject">Subject</label>
-        <select id="subject" name="subject" required>
+        <select id="subject" name="subject" aria-label="subject" required>
           <option value="" disabled>
             Select a subject
           </option>
-          <option value="General Inquiry">General Inquiry</option>
-          <option value="Workshop">Workshop</option>
-          <option value="Question About Appointments">
+          <option value="General Inquiry" aria-label="General Inquiry">
+            General Inquiry
+          </option>
+          <option value="Workshop" aria-label="Workshop">
+            Workshop
+          </option>
+          <option
+            value="Question About Appointments"
+            aria-label="Question About Appointments"
+          >
             Question About Appointments
           </option>
           <option value="Promotional Inquiry">Promotional Inquiry</option>
@@ -47,7 +60,13 @@ export default function Form() {
       </div>
       <div className={styles.ContactForm__row}>
         <label htmlFor="message">Message</label>
-        <textarea id="message" name="message" rows={5} required />
+        <textarea
+          id="message"
+          name="message"
+          rows={5}
+          aria-label="message"
+          required
+        />
         <ValidationError
           prefix="Message"
           field="message"
@@ -55,7 +74,7 @@ export default function Form() {
         />
       </div>
       <input type="text" name="_gotcha" style={{ display: "none" }} />
-      <button type="submit" disabled={state.submitting}>
+      <button type="submit" aria-label="submit" disabled={state.submitting}>
         Send Message
       </button>
     </form>
